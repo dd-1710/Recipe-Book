5baababf764155d276bba45ff3c34f6166d3c970
dd-1710 <divyadurgameesala@gmail.com>
2025-05-06 22:19:59 +0530

    added backend code

---
+++ Recipe-Backend/Recipe-list/recipe.js
@@ -0,0 +1,40 @@
+const exp = require('express');
+const mysqldb = require('mysql2');
+
+const app = exp();
+const port = 4100;
+
+app.use(exp.json());
+
+const db = mysqldb.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'recipe_book'
+})
+
+db.connect(err=>{
+    if(err){
+     console.error("Data is not connected",err);
+     return;
+    }
+    else{
+        console.log('Connected Successfully');
+    }
+},
+
+app.get('/get_recipe',(req,res)=>{
+const sql = "Select * from recipe";
+db.query(sql,(err,result)=>{
+    if(err){
+        console.log("There is a error with the recipe query");
+        return res.status(500).send("Error while fetching recipe data");
+    }
+    let resp = res.json(result);
+    console.log(resp,"response")
+})
+}),
+app.listen(port,()=>{
+ console.log("Service running on ",{port})
+})
+)
